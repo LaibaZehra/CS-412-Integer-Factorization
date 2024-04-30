51a50935beac496c2d636f3be87dc965c58c77fc
Batool-AA <ba07612@st.habib.edu.pk>
2024-04-30 17:13:35 +0500

    pollard.py

--- pollard.py
+++ pollard.py
@@ -1,32 +1,15 @@
-#add pollard rho here (geeks for geeks) - recursive
+#add pollard rho here (geeks for geeks)
 
 # Python 3 program to find a prime factor of composite using
 # Pollard's Rho algorithm
-import random
+
 import math
 
-# Function to calculate (base^exponent)%modulus
-def modular_pow(base, exponent,modulus):
-
-	# initialize result
-	result = 1
-
-	while (exponent > 0):
-
-		# if y is odd, multiply base with result
-		if (exponent & 1):
-			result = (result * base) % modulus
-
-		# exponent = exponent/2
-		exponent = exponent >> 1
-
-		# base = base * base
-		base = (base * base) % modulus
-
-	return result
-
+def func(a):
+	x = a*a + 1
+	return x
 # method to return prime divisor for n
-def PollardRho(n,i):
+def PollardRho(n):
 
 	# no prime divisor for 1
 	if (n == 1):
@@ -36,28 +19,21 @@ def PollardRho(n,i):
 	if (n % 2 == 0):
 		return 2
 
-	# we will pick from the range [2, N)
-	x = (random.randint(0, 2) % (n - 2))
-	y = x
-
-	# the constant in f(x).
-	# Algorithm can be re-run with a different c
-	# if it throws failure for a composite.
-	c = (random.randint(0, 1) % (n - 1))
-
 	# Initialize candidate divisor (or result)
 	d = 1
+	x = 2
+	y = 2
 
 	# until the prime factor isn't obtained.
 	# If n is prime, return n
 	while (d == 1):
 
 		# Tortoise Move: x(i+1) = f(x(i))
-		x = (modular_pow(x, 2, n) + c + n)%n
-
+		x = (func(x))%n
+		
 		# Hare Move: y(i+1) = f(f(y(i)))
-		y = (modular_pow(y, 2, n) + c + n)%n
-		y = (modular_pow(y, 2, n) + c + n)%n
+		y = (func(y))%n
+		y = (func(y))%n
 
 		# check gcd of |x-y| and n
 		d = math.gcd(abs(x - y), n)
@@ -65,23 +41,21 @@ def PollardRho(n,i):
 		# retry if the algorithm fails to find prime factor
 		# with chosen x and c
 		if (d == n):
-			if i <= 3:
-				return PollardRho(n,i+1)
-			else:
-				return n
+			return n
 
-		return d
+	return d
 
 # Driver function
 if __name__ == "__main__":
-  n = 27
-  m = n
-  f = []
-  a = PollardRho(n,1)
-  f.append(a)
-  n = n // a
-  while n != 1:
-    a = PollardRho(n,1)
-    f.append(a)
-    n = n // a
-  print("Factors of", m, "are", f)
+	n = 85768
+	m = n
+	f = []
+	while m != 1:
+		a = PollardRho(m)
+		f.append(a)
+		m = m // a
+		
+	print("Factors of", n, "are", f)
+
+
+# This code is contributed by chitranayal
