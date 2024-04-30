768cbe976049525f99ebfec1db5296b01ada1375
Batool-AA <ba07612@st.habib.edu.pk>
2024-04-30 18:59:00 +0500

    pollard

--- pollard.py
+++ pollard.py
@@ -1,7 +1,12 @@
 import math
+import random
 
 def func(a):
-	x = a*a + 1
+	i = random.randint(0,1)
+	if  i == 0:
+		x = a*a - 1
+	else:
+		x = a*a + 1
 	return x
 
 def PollardRho(n):
@@ -29,21 +34,23 @@ def find_prime_factors(n):
 	f = []
 	while m != 1:
 		a = PollardRho(m)
+		a = PollardRho(a)
 		f.append(a)
 		m = m // a
 	if len(f) == 1:
 		return 'prime'
 	return f
 
-def run_on_range(start, end):
-    results = {}
-    for number in range(start, end + 1):
-        prime_factors = find_prime_factors(number)
-        results[number] = prime_factors
-    return results
+print(find_prime_factors(757686))
+# def run_on_range(start, end):
+#     results = {}
+#     for number in range(start, end + 1):
+#         prime_factors = find_prime_factors(number)
+#         results[number] = prime_factors
+#     return results
 
-results = run_on_range(1, 500)
+# results = run_on_range(1, 500)
 
-for number, prime_factors in results.items():
-    print(f"Number {number}: Prime factors: {prime_factors}")
+# for number, prime_factors in results.items():
+#     print(f"Number {number}: Prime factors: {prime_factors}")
 
